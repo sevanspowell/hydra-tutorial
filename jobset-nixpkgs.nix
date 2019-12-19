df6241dff46030bff5257d8b6a68c841a6935b77
Samuel Evans-Powell <samuel.evans-powell@tiny.cloud>
2019-12-19 13:56:52 +1000

    WIP

--- jobset-nixpkgs.nix
+++ jobset-nixpkgs.nix
@@ -11,10 +11,10 @@ rec {
   hello_world_1 = pkgs_x86_64_linux.hello;
 
   # 'hydraJob' strips all non-essential attributes.
-  hello_world_2 = pkgs.lib.hydraJob pkgs_x86_64_linux.hello;
+  # hello_world_2 = pkgs.lib.hydraJob pkgs_x86_64_linux.hello;
 
   # Generate one attribute per supported platform.
-  hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
+  # hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
 
 } // mapTestOn {
 
