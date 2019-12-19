c2d8430834fb7bd995c5b9cad1af0cd1b3ed1fda
Samuel Evans-Powell <samuel.evans-powell@tiny.cloud>
2019-12-19 12:02:17 +1000

    WIP

--- jobset-nixpkgs.nix
+++ jobset-nixpkgs.nix
@@ -1,11 +1,11 @@
 # nixpkgs-ci.nix
 
-{ supportedSystems ? ["i686-linux" "x86_64-linux"] }:
+{ supportedSystems ? ["i686-linux"] }:
 
 with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
 
 rec {
-  pkgs_x86_64_linux = pkgsFor "x86_64_linux";
+  pkgs_x86_64_linux = pkgsFor "i686-linux";
 
   # Simply assign a derivation to an attribute to have it built.
   hello_world_1 = pkgs_x86_64_linux.hello;
